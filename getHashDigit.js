dfbbccd514ea17168525e158d3d547180175d162
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2022-10-23 13:58:41 -0400

    sdd code excerpt

---
+++ getHashDigit.js
@@ -0,0 +1,5 @@
+export function getHashDigit(hash, placeholder) { // HASH_ROUNDTRIP_VAR2LIT_FIX
+  var varLetter = placeholder.charAt(1); // without preceding $
+  var index = varLetter.charCodeAt() - "A".charCodeAt();
+  return hash.split(' ')[index];
+}
