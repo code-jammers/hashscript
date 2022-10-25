4a9385db764dec4a3d4936f5a7ae2d5480e9e54f
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2022-10-25 07:21:53 -0400

    roundTrip function

--- src/hash.test.js
+++ src/hash.test.js
@@ -2,6 +2,7 @@ import { test } from "uvu";
 import { exec } from "child_process";
 import * as assert from "uvu/assert";
 import hash from "./hash.js";
+import { roundTrip } from "./roundTrip.js";
 
 hash.setHmacKey("hashscripttest");
 
@@ -52,6 +53,42 @@ test("hash substrings test", () => {
 // HASH_NODIGITS_FIX
 test("without digits can hash", () => {
   assert.ok(hash.hashInferPluralDigitCsv("test", "test") != null);
+});
+
+// HASH_ROUNDTRIP_FIX
+test("hash round trip", () => {
+  var hash = "10 11 6";
+  var text = "{test}1 or 2 or 3";
+  var actual = text;
+  try {
+    actual = roundTrip(/*text:*/ text, /*hash:*/ hash, /*dynVars:*/ false);
+  } catch {}
+  var expected = "{test}10 or 11 or 6";
+  assert.is(actual, expected);
+});
+
+// HASH_ROUNDTRIP_LIT2VAR
+test("hash round trip literal to var", () => {
+  var hash = "2 5";
+  var text = "{test}2 or 5";
+  var actual = text;
+  try {
+    actual = roundTrip(/*text:*/ text, /*hash:*/ hash, /*dynVars:*/ true);
+  } catch {}
+  var expected = "{test}$A or $B";
+  assert.is(actual, expected);
+});
+
+// HASH_ROUNDTRIP_VAR2LIT_FIX
+test("hash round trip var to literal", () => {
+  var hash = "2 5";
+  var text = "{test}$A or $B";
+  var actual = text;
+  try {
+    actual = roundTrip(/*text:*/ text, /*hash:*/ hash, /*dynVars:*/ false);
+  } catch {}
+  var expected = "{test}2 or 5";
+  assert.is(actual, expected);
 });
 
 test.run();
